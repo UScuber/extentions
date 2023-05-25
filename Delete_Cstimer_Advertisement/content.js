276feea3ddc5baf04097a6a5a25d4ad95fc9761b
sota <sota1210nittc@gmail.com>
2023-05-25 12:22:59 +0900

    add delete cstimer adv

---
+++ Delete_Cstimer_Advertisement/content.js
@@ -0,0 +1,6 @@
+setInterval(() => {
+  const adv = document.querySelector("body > ins:nth-child(10)");
+  adv?.remove();
+  const adv2 = document.querySelector("body > ins:nth-child(11)");
+  adv2?.remove();
+}, 200);
